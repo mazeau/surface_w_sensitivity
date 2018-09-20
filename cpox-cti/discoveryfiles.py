026b7b3320ecec11b98f5aaf9643e39862e0b6e9
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-20 16:12:16 -0400

    fixing again

--- cpox-cti/discoveryfiles.py
+++ cpox-cti/discoveryfiles.py
@@ -8,7 +8,10 @@ for x in array:
     try:
         shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/chemkin/species_dictionary.txt',
                  './linearscaling/species_dictionary.txt' + x)
+    except:
+        print 'cannot find species_dictionary for %s'%(x)
+    try:
         shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/cantera/chem.cti',
                  './linearscaling/chem.cti' + x)
     except:
-        print '%s is not here'%(array)
+        print 'cannot find cantera file for %s'%(x)
