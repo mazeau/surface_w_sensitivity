eb09a6641e5ebeee11b81b713b794c7437e71fc9
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-20 16:10:43 -0400

    fixed

--- cpox-cti/discoveryfiles.py
+++ cpox-cti/discoveryfiles.py
@@ -5,7 +5,10 @@ array = os.listdir('../../linear-scaling-tests/binding_energies/')
 
 # get the species dictionaries
 for x in array:
-    shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/chemkin/species_dictionary.txt',
+    try:
+        shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/chemkin/species_dictionary.txt',
                  './linearscaling/species_dictionary.txt' + x)
-    shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/cantera/chem.cti',
+        shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/cantera/chem.cti',
                  './linearscaling/chem.cti' + x)
+    except:
+        print '%s is not here'%(array)
