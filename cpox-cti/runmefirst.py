78152e3dcc1e167e4e2373e8fb03b851853a6cef
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-20 16:39:06 -0400

    added exceptions

--- cpox-cti/runmefirst.py
+++ cpox-cti/runmefirst.py
@@ -5,10 +5,13 @@ array = os.listdir('./linearscaling')
 files = ['./linearscaling/' + x for x in array]
 
 for x in files:
-    # copying the copyme file and pasting into each linearscaling folder
-    shutil.copy('copyme.ipynb', x)
-    shutil.copy('run.sh', x)
-    # removing prior error and output logs, as well as old results
-    os.remove('output.log')
-    os.remove('error.log')
-    os.remove('alldone.ipynb')
+    try:
+        # copying the copyme file and pasting into each linearscaling folder
+        shutil.copy('copyme.ipynb', x)
+        shutil.copy('run.sh', x)
+        # removing prior error and output logs, as well as old results
+        os.remove('output.log')
+        os.remove('error.log')
+        os.remove('alldone.ipynb')
+    except:
+	pass
