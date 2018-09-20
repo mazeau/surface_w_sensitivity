f72a5915f8d6c17d30e49f6754c60657373473c9
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-20 16:21:25 -0400

    fixed a mistake

--- cpox-cti/discoveryfiles.py
+++ cpox-cti/discoveryfiles.py
@@ -6,12 +6,10 @@ array = os.listdir('../../linear-scaling-tests/binding_energies/')
 # get the species dictionaries
 for x in array:
     try:
-        shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/chemkin/species_dictionary.txt',
-                 './linearscaling/species_dictionary.txt' + x)
+        shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/chemkin/species_dictionary.txt','./linearscaling/' + x + 'species_dictionary.txt')
     except:
         print 'cannot find species_dictionary for %s'%(x)
     try:
-        shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/cantera/chem.cti',
-                 './linearscaling/chem.cti' + x)
+        shutil.copy2('../../linear-scaling-tests/binding_energies/' + x + '/cantera/chem.cti','./linearscaling/' + x + 'chem.cti)
     except:
         print 'cannot find cantera file for %s'%(x)
