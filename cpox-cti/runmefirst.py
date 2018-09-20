0f949424c1a5105987931a7454310f40637577af
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-20 15:27:06 -0400

    changed runmefirst to delete old files

--- cpox-cti/runmefirst.py
+++ cpox-cti/runmefirst.py
@@ -4,7 +4,11 @@ import shutil
 array = os.listdir('./linearscaling')
 files = ['./linearscaling/' + x for x in array]
 
-# copying the copyme file and pasting into each linearscaling folder
 for x in files:
+    # copying the copyme file and pasting into each linearscaling folder
     shutil.copy('copyme.ipynb', x)
-    shutil.copy('run.sh',x)
+    shutil.copy('run.sh', x)
+    # removing prior error and output logs, as well as old results
+    os.remove('output.log')
+    os.remove('error.log')
+    os.remove('alldone.ipynb')
