0fc4da273b058de2c55e64af7887db47c7b2fe61
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-24 13:09:19 -0400

    added more time

--- cpox-cti/run.sh
+++ cpox-cti/run.sh
@@ -2,7 +2,8 @@
 #SBATCH --job-name=sensitivity
 #SBATCH --error=error.log
 #SBATCH -n1
+#SBATCH --output=output.log
 #SBATCH --mem=10Gb
-#SBATCH --time=5:00:00
+#SBATCH --time=8:00:00
 
-jupyter nbconvert --ExecutePreprocessor.timeout=9999 --to notebook --execute copyme.ipynb --output=alldone.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=30000 --to notebook --execute copyme.ipynb --output=alldone.ipynb
