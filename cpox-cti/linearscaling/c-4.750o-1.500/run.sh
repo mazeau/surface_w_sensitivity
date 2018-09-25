8e39a8d900c9178a73c30a5ae2a2bb44a606c599
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-24 21:53:23 -0400

    reran the big model

--- cpox-cti/linearscaling/c-4.750o-1.500/run.sh
+++ cpox-cti/linearscaling/c-4.750o-1.500/run.sh
@@ -3,6 +3,6 @@
 #SBATCH --error=error.log
 #SBATCH -n1
 #SBATCH --mem=10Gb
-#SBATCH --time=5:00:00
+#SBATCH --time=10:00:00
 
-jupyter nbconvert --ExecutePreprocessor.timeout=9999 --to notebook --execute copyme.ipynb --output=alldone.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=30000 --to notebook --execute copyme.ipynb --output=alldone.ipynb
