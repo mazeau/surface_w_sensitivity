87cbdea30bbbc51fce202a8d2176929f2c3ea5d5
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-02-25 12:26:49 -0500

    still doesn't work

--- cpox-cti/linearscaling/c-7.500o-4.000/run.sh
+++ cpox-cti/linearscaling/c-7.500o-4.000/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --error=error.log
 #SBATCH -n1
 #SBATCH --output=output.log
-#SBATCH --mem=90Gb
+#SBATCH --mem=120Gb
 #SBATCH --time=24:00:00
 
 jupyter nbconvert --ExecutePreprocessor.timeout=40000 --to notebook --execute copyme.ipynb --output=alldone.ipynb
