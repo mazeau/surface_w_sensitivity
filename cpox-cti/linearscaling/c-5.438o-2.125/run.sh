2837c856b2532b964d9f081f4d77bcfd9c1be5d8
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-04-05 02:14:06 -0400

    moving back locally to make plots

--- cpox-cti/linearscaling/c-5.438o-2.125/run.sh
+++ cpox-cti/linearscaling/c-5.438o-2.125/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=output.log
 #SBATCH --exclusive
 #SBATCH --partition=general
-#SBATCH --mem=120Gb
-#SBATCH --time=24:00:00
+#SBATCH --mem=20Gb
+#SBATCH --time=4:00:00
 
 jupyter nbconvert --ExecutePreprocessor.timeout=40000 --to notebook --execute copyme.ipynb --output=alldone.ipynb
