b6c1fab313da6d420a1ceed402e13097cd86c266
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-04-05 13:14:27 -0400

    finishing up

--- cpox-cti/linearscaling/c-4.750o-2.750/run.sh
+++ cpox-cti/linearscaling/c-4.750o-2.750/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=output.log
 #SBATCH --exclusive
 #SBATCH --partition=general
-#SBATCH --mem=100Gb
+#SBATCH --mem=120Gb
 #SBATCH --time=24:00:00
 
 jupyter nbconvert --ExecutePreprocessor.timeout=40000 --to notebook --execute copyme.ipynb --output=alldone.ipynb
