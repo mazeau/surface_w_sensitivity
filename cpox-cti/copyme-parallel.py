ea2cc727bb59c0261f65e68871e953ef21ec552c
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-05-05 17:29:24 -0400

    updating the p with notebook changes

--- cpox-cti/copyme-parallel.py
+++ cpox-cti/copyme-parallel.py
@@ -110,6 +110,7 @@ def monolithFull(gas, surf, temp, mol_in, verbose=False, sens=False):
     ch4, o2, ar = mol_in
     ratio = ch4/(2*o2)
     ratio = round(ratio,1)
+    ratio = str(ratio)
     ch4 = str(ch4)
     o2 = str(o2)
     ar = str(ar)
@@ -197,8 +198,8 @@ def monolithFull(gas, surf, temp, mol_in, verbose=False, sens=False):
                 diagram = ct.ReactionPathDiagram(surf, 'X')
                 diagram.title = 'rxn path'
                 diagram.label_threshold = 1e-9
-                dot_file = out_dir + '/rxnpath-' + str(ratio) + '-x-' + location + 'mm.dot'
-                img_file = out_dir + '/rxnpath-' + str(ratio) + '-x-' + location + 'mm.png'
+                dot_file = out_dir + '/rxnpath-' + ratio + '-x-' + location + 'mm.dot'
+                img_file = out_dir + '/rxnpath-' + ratio + '-x-' + location + 'mm.png'
                 img_path = os.path.join(os.getcwd(), img_file)
                 diagram.write_dot(dot_file)
                 os.system('dot {0} -Tpng -o{1} -Gdpi=200'.format(dot_file, img_file))
@@ -216,8 +217,8 @@ def monolithFull(gas, surf, temp, mol_in, verbose=False, sens=False):
                     diagram = ct.ReactionPathDiagram(surf, element)
                     diagram.title = element + 'rxn path'
                     diagram.label_threshold = 1e-9
-                    dot_file = out_dir + '/rxnpath-' + str(ratio) + '-surf-' + location + 'mm-' + element + '.dot'
-                    img_file = out_dir + '/rxnpath-' + str(ratio) + '-surf-' + location + 'mm-' + element + '.png'
+                    dot_file = out_dir + '/rxnpath-' + ratio + '-surf-' + location + 'mm-' + element + '.dot'
+                    img_file = out_dir + '/rxnpath-' + ratio + '-surf-' + location + 'mm-' + element + '.png'
                     img_path = os.path.join(out_dir, img_file)
                     diagram.write_dot(dot_file)
                     os.system('dot {0} -Tpng -o{1} -Gdpi=200'.format(dot_file, img_file))
