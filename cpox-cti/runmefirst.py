27adeb04df21d6d260749228c462c99b5aa69523
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-05-31 21:43:48 -0400

    first run thru.. I hope.  not sure how many fully ran yet

--- cpox-cti/runmefirst.py
+++ cpox-cti/runmefirst.py
@@ -6,8 +6,8 @@ files = ['./linearscaling/' + x for x in array]
 
 for x in files:
     # copying the copyme file and pasting into each linearscaling folder
-    shutil.copy('copyme.ipynb', x)
-    shutil.copy('run.sh', x)
+    shutil.copy('copyme-parallel.py', x)
+    shutil.copy('runparallel.sh', x)
     #shutil.copy('copyme-fixed-temp-profile.ipynb', x)
     #shutil.copy('run_ft.sh' ,x)
     # removing prior error and output logs, as well as old results
@@ -19,20 +19,17 @@ for x in files:
         os.remove(x + '/error.log')
     except:
         pass
-    try:
-        os.remove(x + '/alldone.ipynb')
-    except:
-        pass
-    # removing past data
-    try:
-	os.remove(x + '/dict.csv')
-    except:
-	pass
-    try:
-	os.remove(x + '/time.txt')
-    except:
-	pass
-    for file in glob.glob(x + "/rxnpath*"):
+    for file in glob.glob(x + '/figures/*.png'):  # remove old plots
         os.remove(file)
-for file in glob.glob("rxnpath*"):
+    for file in glob.glob(x + '/rxnpath/rxnpath*'):  # remove old rxn path diagrams
         os.remove(file)
+    for file in glob.glob(x + '/sensitivities/*.csv'):  # remove old sensitivities
+        os.remove(file)
+    for file in glob.glob(x + '/*.csv'):  # remove old conversions and selectivities csv
+	os.remove(file)
+#for file in glob.glob('/figures/*.png'):
+#    os.remove(file)
+#for file in glob.glob('/rxnpath/rxnpath*'):
+#    os.remove(file)
+#for file in glob.glob('sensivitivies/*.png'):
+#    os.remove(file)
--- cpox-cti/start_all.sh
+++ cpox-cti/start_all.sh
@@ -1,2 +1,4 @@
 export RMGpy=/scratch/westgroup/mazeau/Cat/RMG-Py
-find . -name run.sh -execdir sh -c "sbatch run.sh" \;
+#find . -name run.sh -execdir sh -c "sbatch run.sh" \;
+find . -name runparallel.sh -execdir sh -c "sbatch runparallel.sh" \;
+
