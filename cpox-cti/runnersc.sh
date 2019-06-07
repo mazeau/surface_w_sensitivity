b2b28d0803f7d437c8374a9153141a1490cd9dcd
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-06-07 16:00:35 -0700

    debug queue takes much longer to finish jobs vs discovery, so trying on regular now

--- cpox-cti/runnersc.sh
+++ cpox-cti/runnersc.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-#SBATCH --qos=debug
+#SBATCH --qos=regular
 #SBATCH --constraint=knl
 #SBATCH --job-name=sensitivity
 #SBATCH --error=error.log
 #SBATCH --output=output.log
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=15
-#SBATCH --mem=5Gb
-#SBATCH --time=0:20:00
-#SBATCH --mail-type=begin,end,fail
-#SBATCH --mail-user=mazeau.e@husky.neu.edu
+#SBATCH --mem-per-cpu=1Gb
+#SBATCH --time=1:00:00
 
 source activate rmg_env
 python copyme-parallel.py
