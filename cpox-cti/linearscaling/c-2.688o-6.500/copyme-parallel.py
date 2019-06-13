f7314bbddbb267d821f23c97701a427d0ff1d0d3
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-06-12 22:19:52 -0400

    running at different sensitivities:

--- cpox-cti/linearscaling/c-2.688o-6.500/copyme-parallel.py
+++ cpox-cti/linearscaling/c-2.688o-6.500/copyme-parallel.py
@@ -430,7 +430,7 @@ def monolithFull(gas, surf, temp, mol_in, verbose=False, sens=False):
 
     # set relative and absolute tolerances on the simulation
     sim.rtol = 1.0e-10
-    sim.atol = 1.0e-19
+    sim.atol = 1.0e-20
 
     gas_names = gas.species_names
     surf_names = surf.species_names
