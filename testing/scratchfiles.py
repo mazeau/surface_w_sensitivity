f3b066cf8e616cbf308ccd574ea87e7ac9013126
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-07-18 22:36:46 -0400

    new rerun of smaller grid

--- testing/scratchfiles.py
+++ testing/scratchfiles.py
@@ -1,21 +1,21 @@
 import os
 import shutil
 
-array = os.listdir('../../../linear-scaling-tests/cpox/binding_energies/')
+array = os.listdir('../../../linear-scaling-tests/cpox2/binding_energies/')
 #array = os.listdir('/scratch/westgroup/mazeau/linear-scaling-tests/cpox/binding_energies/')
 
 # get the species dictionaries
 for x in array:
     try:
-        shutil.copy2('../../../linear-scaling-tests/cpox/binding_energies/' + x + '/chemkin/species_dictionary.txt','./linearscaling/' + x + '/species_dictionary.txt')
+        shutil.copy2('../../../linear-scaling-tests/cpox2/binding_energies/' + x + '/chemkin/species_dictionary.txt','./linearscaling/' + x + '/species_dictionary.txt')
     except:
         print 'cannot find species_dictionary for %s'%(x)
     try:
-        shutil.copy2('../../../linear-scaling-tests/cpox/binding_energies/' + x + '/cantera/chem_annotated.cti','./linearscaling/' + x + '/chem_annotated.cti')
+        shutil.copy2('../../../linear-scaling-tests/cpox2/binding_energies/' + x + '/cantera/chem_annotated.cti','./linearscaling/' + x + '/chem_annotated.cti')
     except:
         print 'cannot find cantera file for %s'%(x)
 try:
-    shutil.copy2('../../../linear-scaling-tests/cpox/base/chemkin/species_dictionary.txt','./species_dictionary.txt')
-    shutil.copy2('../../../linear-scaling-tests/cpox/base/cantera/chem_annotated.cti','./chem_annotated.cti')
+    shutil.copy2('../../../linear-scaling-tests/cpox2/base/chemkin/species_dictionary.txt','./species_dictionary.txt')
+    shutil.copy2('../../../linear-scaling-tests/cpox2/base/cantera/chem_annotated.cti','./chem_annotated.cti')
 except:
     print 'cannot find files for base'
--- testing/start_all.sh
+++ testing/start_all.sh
@@ -1,4 +1,5 @@
 export RMGpy=/scratch/westgroup/mazeau/Cat/RMG-Py
 #find . -name run.sh -execdir sh -c "sbatch run.sh" \;
+source activate rmg_env3
 find . -name runparallel.sh -execdir sh -c "sbatch runparallel.sh" \;
 
