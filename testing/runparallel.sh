b7dddca3c8926cb3446067668c625b8771a1c36a
Emily Mazeau <mazeau.e@husky.neu.edu>
2020-01-09 11:44:02 -0500

    trying to run methanol but it does not combust yet

--- testing/runparallel.sh
+++ testing/runparallel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=sensitivity
+#SBATCH --job-name=methanol
 #SBATCH --error=error.log
 #SBATCH --output=output.log
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH --partition=west,general
+#SBATCH --partition=west,general,test,interactive
 #SBATCH --exclude=c5003
-#SBATCH -c 15
+#SBATCH -c 6
 #SBATCH --mem-per-cpu=4Gb
-#SBATCH --time=5:00:00
+#SBATCH --time=00:10:00
 
-source activate rmg_env3
+source activate rmg_env
 python copyme-parallel.py
 #python copymetest.py
